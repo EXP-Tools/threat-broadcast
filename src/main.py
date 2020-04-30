a98d942beac7fe2f09e9f2a34edef4a9e1d83450
EXP <289065406@qq.com>
2020-04-30 22:25:24 +0800

    修正 MD5 算法

--- src/main.py
+++ src/main.py
@@ -19,6 +19,13 @@ from src.crawler.anquanke import AnQuanKe
 from src.crawler.vas import Vas
 
 
+def help_info():
+    return '''
+    -h                帮助信息
+    -top <number>     播报时每个来源最多取最新的前 N 个 CVE （默认 10）
+'''
+
+
 def init():
     reload(sys)
     sys.setdefaultencoding(env.CHARSET)
@@ -30,22 +37,26 @@ def init():
 
 
 
-def main(a, b, c):
-    srcs = [ Cert360(), Nsfocus(), QiAnXin(), RedQueen(), AnQuanKe(), Vas() ]
-    for src in srcs:
-        msgs = src.cve_msgs()
-        to_log(msgs)
-        to_page(msgs)
-        to_notice(msgs)
+def main(help, top):
+    if help:
+        log.info(help_info())
+
+    else:
+        srcs = [ Cert360(), Nsfocus(), QiAnXin(), RedQueen(), AnQuanKe(), Vas() ]
+        for src in srcs:
+            msgs = src.cve_msgs()
+            to_log(msgs)
+            to_page(msgs, top)
+            to_notice(msgs)
 
 
 def to_log(msgs):
     map(lambda msg : log.info(msg), msgs)
 
 
-def to_page(msgs, top_limit = 10):
+def to_page(msgs, top):
     if msgs:
-        page.to_page(top_limit)
+        page.to_page(top)
 
 
 def to_notice(msgs):
@@ -54,29 +65,24 @@ def to_notice(msgs):
 
 
 def get_sys_args(sys_args) :
-    a = ''
-    b = ''
-    c = ''
+    help = False
+    top = 10
 
     idx = 1
     size = len(sys_args)
     while idx < size :
         try :
             if sys_args[idx] == '-h' :
-                idx += 1
-                a = int(sys_args[idx])
+                help = True
 
-            elif sys_args[idx] == '-m' :
+            elif sys_args[idx] == '-top' :
                 idx += 1
-                b = int(sys_args[idx])
+                top = int(sys_args[idx])
 
-            elif sys_args[idx] == '-s' :
-                idx += 1
-                c = int(sys_args[idx])
         except :
             pass
         idx += 1
-    return a, b, c
+    return help, top
 
 
 if __name__ == '__main__':
